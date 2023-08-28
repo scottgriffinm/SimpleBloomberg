e8f1a5807904f0661805628a9efa6de74a4d4d6a
Scott Griffin <scottgriffinm@gmail.com>
2023-08-27 19:47:18 -0500

    Created SimpleBloomberg.py, gets historical prices

---
+++ SimpleBloomberg.py
@@ -0,0 +1,60 @@
+import blpapi
+'''
+SimpleBloomberg: A simple wrapper for the Bloomberg API
+
+Written by Scott Griffin
+scottgriffinm@gmail.com
+'''
+
+def __startSession():
+    d_host = 'localhost'
+    d_port = 8194
+    sessionOptions = blpapi.SessionOptions()
+    sessionOptions.setServerHost(d_host)
+    sessionOptions.setServerPort(d_port)
+    session = blpapi.Session(sessionOptions)
+    session.start()
+    return session
+
+def getHistoricalPrices(securities, start_date, end_date, frequency, fields='PX_LAST'):
+    '''
+    :param securities: string or list of strings in format TICKER EXCHANGE ex: 'IBM US Equity'
+    :param start_date: string in format YYYYMMDD
+    :param end_date: string in format YYYYMMDD
+    :param frequency: string in 'DAILY', 'WEEKLY', 'MONTHLY', 'QUARTERLY', 'SEMI_ANNUALLY', 'YEARLY'
+    :param fields: list of strings, type of price data to get from bloomberg
+    '''
+    assert frequency in ['DAILY', 'WEEKLY', 'MONTHLY', 'QUARTERLY', 'SEMI_ANNUALLY', 'YEARLY']
+    session = __startSession()
+    session.openService('//blp/refdata')
+    refDataService = session.getService('//blp/refdata')
+    request = refDataService.createRequest('HistoricalDataRequest')
+    if type(securities) == str:
+        securities = [securities]
+    for security in securities:
+        request.getElement('securities').appendValue(security)
+    if type(fields) == str:
+        fields = [fields]
+    for field in fields:
+        request.getElement('fields').appendValue(field)
+    request.set('periodicitySelection', frequency)
+    request.set('startDate', start_date)
+    request.set('endDate', end_date)
+    session.sendRequest(request)
+    data = {}
+    while True:
+        event = session.nextEvent()
+        for msg in event:
+            if msg.messageType() == 'HistoricalDataResponse':
+                dates = []
+                prices = []
+                securityData = msg.getElement('securityData')
+                fieldData = securityData.getElement('fieldData')
+                for element in fieldData:
+                    dates.append(element.getElementAsString('date'))
+                    prices.append(element.getElementAsFloat('PX_LAST'))
+                data[securityData.getElementAsString('security')] = {'dates': dates, 'prices': prices}
+        if event.eventType() == blpapi.Event.RESPONSE:
+            break
+
+    return data
